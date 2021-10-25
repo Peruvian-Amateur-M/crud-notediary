5c1cf961411f1bd32a915c630099ec53d6923501
AndréS <andresp@gmail.com>
2021-10-25 12:22:38 -0500

    creacion inicial

---
+++ diario-de-notas/src/main/java/com/first/project/diariodenotas/DiarioDeNotasApplication.java
@@ -0,0 +1,13 @@
+package com.first.project.diariodenotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioDeNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioDeNotasApplication.class, args);
+	}
+
+}
---
+++ diario-de-notas/src/test/java/com/first/project/diariodenotas/DiarioDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.project.diariodenotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
